b7c651e2574de7695a257a0925376a4d3443cb5b
mohit032002 <mohitchabriya@skyvisionitsolutions.in>
2025-10-09 00:21:34 +0530

    Fix Firebase config for web

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -5,7 +5,7 @@ class DefaultFirebaseOptions {
     apiKey: "AIzaSyBwvhBePuqMs1Ze1SN_w4ggz7f18oLJn1c",
     authDomain: "menu-scan-web.firebaseapp.com",
     projectId: "menu-scan-web",
-    storageBucket: "menu-scan-web.firebasestorage.app",
+    storageBucket: "menu-scan-web.appspot.com", // ✅ fixed here
     messagingSenderId: "8774055269",
     appId: "1:8774055269:web:29d073805a4653d0539d8c",
   );
